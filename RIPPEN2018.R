510cb851de684a04f86e8b8f0826822117157aec
Donald Stolz <donaldtstolz@gmail.com>
2018-01-25 18:32:59 -0600

    While loop for downs; Only check for interception on incomplete pass.

--- RIPPEN2018.R
+++ RIPPEN2018.R
@@ -1,7 +1,8 @@
-nfl<-read.csv("~/Dropbox/RIPPEN/NFL by Play 2009-2016 (v2).csv")
 
-temp <- subset(nfl,Passer=="J.Cutler",select= c("Passer","PassOutcome","AirYards","YardsAfterCatch","InterceptionThrown","Fumble"))
 
+nfl<-read.csv("~/RIPPEN/NFL-Play-by-Play-2009-16.csv")
+
+temp <- subset(nfl,Passer=="T.Brady",select= c("Passer","PassOutcome","AirYards","YardsAfterCatch","InterceptionThrown","Fumble"))
 
 
 ###################################################
@@ -16,34 +17,36 @@ drivesim <- function(qbdata){
 
 	#Add a while loop to make sure that down is always less than 4.
 	while(driveState$down < 4){
-		#Was the pass intercepted
-		int <- sample(qbdata$InterceptionThrown,1)
-		if (int == 1){return(0)}
-		if (int == 0){
-		  #Returns 1 if complete and 0 if incomplete
-		  comp <- (sample(qbdata$PassOutcome,1)=="Complete")+0
-			# If incomplete add down
-		  if (comp == 0){ driveState$down <- driveState$down + 1 }
-			# Else get results of completed pass
-			else {
-		    yards <- sample(qbdata$AirYards[qbdata$PassOutcome=="Complete"],1)
-		    if (driveState$togo-yards>0){
-		      driveState$down <- driveState$down + 1
-		      driveState$togo <- driveState$togo - yards
-		    } else
-		    {
-		      driveState$down <- 1
-		      driveState$togo <- 10
-		    }
-
-				# Subtract yards left to score and check for TD
-		    driveState$togoTD <- driveState$togoTD - yards
-				if(driveState$togoTD !> 0){ return (7) }
+	  #Returns 1 if complete and 0 if incomplete
+	  pass <- (sample(qbdata$PassOutcome,1)=="Complete")+0
+		# If incomplete check for interception or add down
+	  if (pass == 0){
+			# Was the pass intercepted, only sampling from incomplete passes
+			int <- sample(qbdata$InterceptionThrown[qbdata$PassOutcome=="Incomplete Pass"],1)
+			if (int == 1){return(0)}
+	    driveState$down <- driveState$down + 1
+	  }
+		# Else get results of completed pass
+		else {
+	    yards <- sample(qbdata$AirYards[qbdata$PassOutcome=="Complete"],1)
+			# Check for first down
+	    if (driveState$togo-yards <= 0){
+				driveState$down <- 1
+	      driveState$togo <- 10
+	    } else {
+				driveState$down <- driveState$down + 1
+				driveState$togo <- driveState$togo - yards
 	    }
-		}
+			# Subtract yards left to score and check for TD
+	    driveState$togoTD <- driveState$togoTD - yards
+			if(driveState$togoTD <= 0){ return (7) }
+    }
 	}
 	#If down is 4 check to see if within field goal range
-	# TODO: More accurate way to calculate field goal for greater range
-	if(driveState$togoTD < 20){return(3)}
+	if(driveState$togoTD < 30){
+		# TODO: More accurate way to calculate field goal for greater range
+		return(3)
+	}
 	else{return(0)}
 }
+drivesim(qbdata)
