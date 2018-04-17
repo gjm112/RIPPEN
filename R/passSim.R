5e10ee96608903a5f23ae40b33c603b914ea6982
Donald Stolz <donaldtstolz@gmail.com>
2018-04-17 10:34:41 -0500

    Add desrciptions

--- R/passSim.R
+++ R/passSim.R
@@ -1,19 +1,19 @@
 #' @title passSim
-#' 
-#' @description What does this do?  
-#' 
+#'
+#' @description This function returns the yardage of single completed pass for RIPPEN  
+#'
 #' @details Fill in the details
-#' 
+#'
 #' @param nu_0 prior degrees of freedom
 #' @param kappa_0 prior something
-#' 
-#' @return 
-#' 
+#'
+#' @return
+#'
 #' @export
 
 
 passSim <- function(qbdata, kappa_0, nu_0){
-  
+
   # 1. League Yardage / Completed Passes
   #Yards for every complete catch
   x <- passPlays$TotalYards[passPlays$PassOutcome=="Complete"]
@@ -28,7 +28,7 @@ passSim <- function(qbdata, kappa_0, nu_0){
   #nu_0 <- 3
   # Scale Parameter
   sigma2_0 <- var(y)
-  
+
   # 2.	Vn <- Vo + nj
   # 		S2n <- Vo*S2o + (nj-1)S2j + [(Ko*nj)/(Ko+nj)](Yj - Mo)^2
   # 		Mn <- (Ko/Ko+nj)*Mo + (nj/Ko+nj)*(Yj)
@@ -41,24 +41,24 @@ passSim <- function(qbdata, kappa_0, nu_0){
     S2_j <- 0
   }
   nu_n <- nu_0 + n_j
-  
+
   sigma2_n <- (1/nu_n)*(nu_0*sigma2_0 + (n_j-1)*S2_j + (kappa_0*n_j)/(kappa_0+n_j)*(ybar_j - mu_0)^2)
-  
-  
+
+
   #kappa_n <- kappa_0 + n_j
-  
+
   # 3.	Draw S2i
   sigma2_star <- rinvgamma(1, shape = nu_n/2, rate = (nu_n/2)*sigma2_n)
   #rinvchisq(1, nu_n, sigma2_n)
   # 4.	Draw Mi
   # 		Mi <- S2i * Yj * N(Mn, S2i/Kn)
   mu_n <- ((kappa_0/sigma2_star)*mu_0 + (n_j/sigma2_star)*ybar_j) / (kappa_0/sigma2_star + n_j/sigma2_star )
-  
+
   mu_star <- rnorm(1, mu_n, sqrt(1 / (kappa_0/sigma2_star + n_j/sigma2_star)))
-  
+
   # 5.	Draw y
   y_tilde <- rnorm(1, mu_star, sqrt(sigma2_star))
-  
+
   # 6.	yards <- exp(Yi - 1)
   yards <- exp(y_tilde) - 1
   return(yards)
--- R/runSim.R
+++ R/runSim.R
@@ -1,23 +1,24 @@
 #' @title runSim
-#' 
-#' @description What does this do?  
-#' 
+#'
+#' @description This function simulates a number of drives for a given passer
+#'
 #' @details Fill in the details
-#' 
+#'
 #' @param nu_0 prior degrees of freedom
 #' @param kappa_0 prior something
-#' 
-#' @return 
-#' 
+#'
+#' @return
+#'
 #' @export
 
 
-
+# NOTE: Consider changing to reflect season toatal nsim=176
+# - 16 games/season * 11 drives/game = 176 drives/season
 runSim <-
 function(passer, kappa_0=1, nu_0=3, nsim = 100){
   print(passer)
   qbdata <- subset(passPlays, Passer==passer)
   results <- replicate(nsim,driveSim(qbdata,kickCoef, kappa_0, nu_0))
-  
+
   return(results)
 }
