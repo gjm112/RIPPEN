9d527a4a08fa739e622677d160d9ccd66a3a0faa
donald-stolz <donaldtstolz@gmail.com>
2018-05-23 11:38:07 -0500

    added date and season parameter

--- R/runSim.R
+++ R/runSim.R
@@ -12,10 +12,25 @@
 #' @export
 
 runSim <-
-function(passer, kappa_0=1, nu_0=3, nsim = 100){
+function(passer, kappa_0=1, nu_0=3, nsim = 100, date=NULL, season=NULL){
   print(passer)
-  qbdata <- subset(passPlays, Passer==passer)
+  
+    if(!is.null(date)){
+    # modify Passplays to contain only results from date
+      gamePasses <- subset(passPlays, Date==date)
+      qbdata <- subset(gamePasses, Passer==passer)
+      
+    } 
+    else if(!is.null(season)){
+    # modify Passplays to contain only results from season
+      seasonPasses <- subset(passPlays, Season==season)
+      qbdata <- subset(seasonPasses, Passer==passer)
+      
+    }
+    else{
+      qbdata <- subset(passPlays, Passer==passer)
+      
+    }
   results <- replicate(nsim,driveSim(qbdata,kickCoef, kappa_0, nu_0))
-
   return(results)
 }
