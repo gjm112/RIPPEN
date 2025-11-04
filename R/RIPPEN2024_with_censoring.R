228c3e75b907a2f1c339ba62e7fcc02d4b8bcd86
Don Stolz <donaldtstolz@gmail.com>
2025-11-04 11:28:21 -0600

    task: Update gatherData function to use nflreadr package

--- R/RIPPEN2024_with_censoring.R
+++ R/RIPPEN2024_with_censoring.R
@@ -3,15 +3,16 @@ library(parallel)
 library(ggplot2)
 library(plotly)
 library(teamcolors)
-source("./R/gatherData.r")
-source("./R/kickCoef.r")
+source("R/kickCoef.R")
+source("R/gatherPassPlays.R")
 
 # Gather data from 2009-2024 seasons
 # Check if data has already been gathered
 data_file_path <- "./data/total09-24_pbp_data.csv"
-first_time <- file.exists(data_file_path)
+first_time <- !file.exists(data_file_path)
 if(first_time == TRUE){
   # Gather data from scratch
+  source("./R/gatherData.R")
   print("Data file not found. Gathering data from scratch...")
   nfl <- gatherData()  
   write.csv(nfl, data_file_path, append = FALSE, row.names = FALSE)
